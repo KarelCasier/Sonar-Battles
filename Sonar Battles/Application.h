522b3b98f3930ef59e401f7741e52efb8d4571b6
KarelCasier <KarelCasier@me.com>
2014-07-07 15:36:04 -0400

    Added Game Structure
    Game States, Resource Holder

---
+++ Sonar Battles/Application.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <SFML/System/Time.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Text.hpp>
+
+#include "ResourceHolder.h"
+#include "ResourceIdentifiers.h"
+#include "StateStack.hpp"
+
+#include <sstream>
+
+class Application
+{
+public:
+							Application();
+	void					run();
+
+private:
+	void					processEvents();
+	void					update(sf::Time dtTime);
+	void					render();
+
+	void					updateStatistics(sf::Time elapsedTime);
+	void					registerStates();
+
+private:
+	//For FPS calculations and consistent movement
+	static const sf::Time	TimePerFrame;
+
+	sf::RenderWindow		mWindow;
+	TextureHolder			mTextures;
+	FontHolder				mFonts;
+
+	StateStack				mStateStack;
+
+	sf::Text				mStatisticsText;
+	sf::Time				mStatisticsUpdateTime;
+	std::size_t				mStatisticsNumFrames;
+
+};
---
+++ Sonar Battles/GameState.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "State.h"
+#include "World.h"
+
+class GameState : public State
+{
+public:
+							GameState(StateStack& stack, Context context);
+
+	virtual void			draw();
+	virtual bool			update(sf::Time dt);
+	virtual bool			handleEvent(const sf::Event& event);
+
+private:
+
+	World					mWorld;
+};
